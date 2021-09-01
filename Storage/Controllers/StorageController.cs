125852b23c616bf187694db4a0c9741a939c5d13
Alexander Azzopardi <87972109+AlexanderAzzopardi@users.noreply.github.com>
2021-09-01 11:45:10 +0100

    Add files via upload

---
+++ Storage/Controllers/StorageController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dapr.Client;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using Storage.Models;
+
+namespace Storage.Controllers
+{
+    [ApiController]
+    public class StorageController : ControllerBase
+    {
+        [HttpGet("add")]
+        public async void AddData(Data dataStored, [FromServices] DaprClient client)
+        {
+            await client.SaveStateAsync("statestore", dataStored.key, dataStored.data);
+            Console.WriteLine($"Data {dataStored.data} stored at Key {dataStored.key}.");
+        }
+
+        [HttpGet("delete")]
+        public async void DeleteData(Data dataStored, [FromServices] DaprClient client)
+        {    
+            dataStored.data = await client.GetStateAsync<string>("statestore", dataStored.key);
+            if(dataStored.data != null)
+            {
+                await client.DeleteStateAsync("statestore", dataStored.key);
+                Console.WriteLine($"Data {dataStored.data} deleted from Key {dataStored.key}.");
+            }
+            else{Console.WriteLine($"There is no data at Key {dataStored.key}.");}
+        }
+        
+        [HttpGet("get")]
+        public async void GetData(Data dataStored, [FromServices] DaprClient client)
+        {  
+            dataStored.data = await client.GetStateAsync<string>("statestore", dataStored.key);
+            if(dataStored.data != null)
+            {
+                Console.WriteLine($"Data {dataStored.data} recieved from Key {dataStored.key}.");
+            }
+            else{Console.WriteLine($"There is no data at Key {dataStored.key}.");}
+        }
+    }
+}
---
+++ Storage/Models/Data.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace Storage.Models
+{
+    public class Data
+    {
+        public string key {get; set;}
+        public string data {get; set;}
+    }
+}
